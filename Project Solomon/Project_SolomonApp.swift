3a0b6c6633673b5a6208147d72e821af7d748bd6
CZandt <cole0hardy@gmail.com>
2023-04-28 14:23:34 -0600

    Initial Commit

---
+++ Project Solomon/Project_SolomonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_SolomonApp.swift
+//  Project Solomon
+//
+//  Created by Cole Hardy on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_SolomonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
